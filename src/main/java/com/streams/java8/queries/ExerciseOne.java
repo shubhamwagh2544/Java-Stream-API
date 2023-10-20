c50fba5a91af67e104d62934858c9eebf3885c94
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-10-20 18:10:06 +0530

    JPQL query-1

--- src/main/java/com/streams/java8/queries/ExerciseOne.java
+++ src/main/java/com/streams/java8/queries/ExerciseOne.java
@@ -2,10 +2,31 @@ package com.streams.java8.queries;
 
 import com.streams.java8.beans.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// 1. Find people aged less or equal 18
+// 2. Then change implementation to find first 10 people
 
 @Repository
 public interface ExerciseOne extends JpaRepository<Person, Integer> {
 
+    @Query(
+            value = """
+                    select *
+                    from person
+                    where age <= ?1
+                    """,
+            nativeQuery = true
+    )
+    List<Person> selectAllPersonWhoseAgeIsLessThan(int age);
+
+    @Query(
+            value = """
+                    
+                    """
+    )
+    List<Person> findAllByAgeLessThanEqual(int age);
 }
--- src/test/java/com/streams/java8/exercise/ExerciseOne.java
+++ src/test/java/com/streams/java8/exercise/ExerciseOne.java
@@ -5,19 +5,42 @@ import com.streams.java8.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // 1. Find people aged less or equal 18
 // 2. Then change implementation to find first 10 people
+
 class ExerciseOne {
     @Test
     void imperativeApproach() throws IOException {
         List<Person> personList = MockData.getPeople();
+        List<Person> youngerPersonList = new ArrayList<>();
+        int limit = 10;
 
+        for (Person person : personList) {
+            if (person.getAge() <= 18) {
+                youngerPersonList.add(person);
+                limit--;
+            }
+            if (limit == 0) break;
+
+        }
+
+        //print
+        System.out.println(youngerPersonList.size());
+        for (Person person : youngerPersonList) {
+            System.out.println(person);
+        }
     }
 
     @Test
     void declarativeApproachUsingStreams() throws Exception {
-
+        MockData.getPeople().stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .forEach(System.out::println);
     }
+
 }
